20eb92cdce768434150bb0ef5e0669a7fe129404
Therapho <51427300+Therapho@users.noreply.github.com>
2024-05-16 06:58:46 -0400

    Added sample data

---
+++ TaskMeta.Data/Sample/dbo.TaskDefinitions.data.sql
@@ -0,0 +1,9 @@
+SET IDENTITY_INSERT [dbo].[TaskDefinitions] ON
+INSERT INTO [dbo].[TaskDefinitions] ([Id], [Description], [Value], [Sequence], [Active], [UserId]) VALUES (3, N'Be On Time', CAST(0.2000 AS Money), 1, 1, N'867e697f-138e-427e-901b-9a71807c670c')
+INSERT INTO [dbo].[TaskDefinitions] ([Id], [Description], [Value], [Sequence], [Active], [UserId]) VALUES (4, N'Care For Dog', CAST(0.2000 AS Money), 2, 1, N'867e697f-138e-427e-901b-9a71807c670c')
+INSERT INTO [dbo].[TaskDefinitions] ([Id], [Description], [Value], [Sequence], [Active], [UserId]) VALUES (5, N'Help With Dinner', CAST(0.2000 AS Money), 3, 1, N'867e697f-138e-427e-901b-9a71807c670c')
+INSERT INTO [dbo].[TaskDefinitions] ([Id], [Description], [Value], [Sequence], [Active], [UserId]) VALUES (6, N'Keep House Tidy', CAST(0.2000 AS Money), 4, 1, N'867e697f-138e-427e-901b-9a71807c670c')
+INSERT INTO [dbo].[TaskDefinitions] ([Id], [Description], [Value], [Sequence], [Active], [UserId]) VALUES (7, N'Be Respectful', CAST(0.2000 AS Money), 5, 1, N'867e697f-138e-427e-901b-9a71807c670c')
+INSERT INTO [dbo].[TaskDefinitions] ([Id], [Description], [Value], [Sequence], [Active], [UserId]) VALUES (8, N'Care for self', CAST(0.2000 AS Money), 6, 1, N'867e697f-138e-427e-901b-9a71807c670c')
+INSERT INTO [dbo].[TaskDefinitions] ([Id], [Description], [Value], [Sequence], [Active], [UserId]) VALUES (9, N'Extra Help', CAST(0.5500 AS Money), 7, 1, N'867e697f-138e-427e-901b-9a71807c670c')
+SET IDENTITY_INSERT [dbo].[TaskDefinitions] OFF
