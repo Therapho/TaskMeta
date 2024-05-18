ae848418b4ba7e8d2ac41c55033a68ca128ab3af
Therapho <51427300+Therapho@users.noreply.github.com>
2024-05-18 18:14:58 -0400

    Add more sample data

---
+++ TaskMeta.Data/Sample/dbo.AspNetRoles.data.sql
@@ -0,0 +1,2 @@
+INSERT INTO [dbo].[AspNetRoles] ([Id], [Name], [NormalizedName], [ConcurrencyStamp]) VALUES (N'1', N'Admin', N'Admin', NULL)
+INSERT INTO [dbo].[AspNetRoles] ([Id], [Name], [NormalizedName], [ConcurrencyStamp]) VALUES (N'2', N'Contributor', N'Contributor', NULL)
---
+++ TaskMeta.Data/Sample/dbo.AspNetUserRoles.data.sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'52582c92-d6ee-46b1-bf3e-fbe4110a2af9', N'1')
+INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'04d145b8-7336-4386-81ab-587172e1cd55', N'2')
+INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'172ade09-d473-49f4-b95b-65778d20ca3f', N'2')
